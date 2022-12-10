c4a9b99f1da87647f435a3feedebe5b291c11b17
kasun gamage <kasun.thaksala@gmail.com>
2022-12-10 10:11:03 +0530

    Update settings.dart

--- lib/config/settings.dart
+++ lib/config/settings.dart
@@ -1,8 +1,8 @@
 //Google PlacesAutocomplete
-const PlacesAutocompleteApiKey = 'AIzaSyBF9jFWJIf9XMu1YbYj03UiC7WMo7cgxAE';
-const GoogleServiceApi = 'AIzaSyBF9jFWJIf9XMu1YbYj03UiC7WMo7cgxAE';
+const PlacesAutocompleteApiKey = '';
+const GoogleServiceApi = '';
 //giphy api
-const GiphyApi_key = "id3MG5utNNq0JmIXxTGdaOMOkBFOGHeh";
+const GiphyApi_key = "";
 
 const Null_user =
-    "https://firebasestorage.googleapis.com/v0/b/nearbyse-d334c.appspot.com/o/profilephoto.png?alt=media&token=e670c422-4717-48dc-af01-ee1fc3e829ce";
+    "";
